d8f18a5f52596654067efe70fb7900f9b711f4d8
Danilo <54450923+Daminhao@users.noreply.github.com>
2021-07-06 22:09:42 -0300

    Update front.py

--- front.py
+++ front.py
@@ -26,7 +26,7 @@ if img:
     np.set_printoptions(suppress=True)
 
     # Load the model
-    model = tensorflow.keras.models.load_model['/saved_model.h5']
+    model = tensorflow.keras.models.load_model('/saved_model.h5')
     # Create the array of the right shape to feed into the keras model
     # The 'length' or number of images you can put into the array is
     # determined by the first position in the shape tuple, in this case 1.
