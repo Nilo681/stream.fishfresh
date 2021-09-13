ce52c79f3064c3b5e9e25d171b3bce9f63204156
Danilo <54450923+Daminhao@users.noreply.github.com>
2021-09-13 10:14:36 -0300

    i
    i

--- front.py
+++ front.py
@@ -69,7 +69,7 @@ if img:
     maior = np.argmax(prediction)
     predicted_class = classes[maior]
     
-    st.text(font-size: 42px, predicted_class)
+    st.markdown(predicted_class)
     
     print(predicted_class)
     #return predicted_class
